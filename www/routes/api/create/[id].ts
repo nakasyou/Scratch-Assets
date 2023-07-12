135f1401f28e06c87f251cfa204bbd9af087963e
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-12 17:30:39 +0900

    Update [id].ts

--- www/routes/api/create/[id].ts
+++ www/routes/api/create/[id].ts
@@ -1,5 +1,31 @@
 import { HandlerContext } from "$fresh/server.ts"
 
-export const handler = (req: Request, ctx: HandlerContext): Response => {
-  return new Response(ctx.params.id)
+export const handler = async (req: Request, ctx: HandlerContext): Response => {
+  const id = ctx.params.id
+  const projectData = await fetch(`https://trampoline.turbowarp.org/api/projects/${id}`).then(res=>res.json())
+  
+  const token = projectData.project_token
+
+  const projectJson = await fetch(`https://projects.scratch.mit.edu/${id}?token=${token}`).then(res=>res.json())
+  let assets = [];
+  function assetsSearch(arg) {
+    if (arg === null) {
+      return
+    }
+    Object.keys(arg).forEach((key) => {
+      if (typeof arg[key] === "object") {
+        if (key === "sounds" || key === "costumes") {
+          const data = arg[key]
+          data.forEach((elem) => {
+            elem.type = key
+          });
+          assets = assets.concat(data)
+        } else {
+          assetsSearch(arg[key])
+        }
+      }
+    })
+  }
+  assetsSearch(projectJson)
+  return new Response(JSON.stringify(assets))
 }
